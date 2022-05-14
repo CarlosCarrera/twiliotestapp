de5a38e4ddcae8515ef99651879c6372cd65646f
Carlos Carrera <carlos.carrera.sge@mango.com>
2022-05-14 11:16:37 +0200

    Initial Commit

---
+++ twilioTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  twilioTestApp
+//
+//  Created by Carlos Carrera on 14/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twilioTestApp/twilioTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  twilioTestAppApp.swift
+//  twilioTestApp
+//
+//  Created by Carlos Carrera on 14/5/22.
+//
+
+import SwiftUI
+
+@main
+struct twilioTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
