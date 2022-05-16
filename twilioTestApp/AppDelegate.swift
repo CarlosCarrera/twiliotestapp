6f848039e165c462cff1f0f5c8af039ba776ffe4
Carlos Carrera <carlos.carrera.sge@mango.com>
2022-05-16 10:00:29 +0200

    first commit

---
+++ twilioTestApp/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  twilioTestApp
+//
+//  Created by Carlos Carrera on 14/5/22.
+//
+
+import Foundation
---
+++ twilioTestApp/ContentPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  ContentPresenter.swift
+//  twilioTestApp
+//
+//  Created by Carlos Carrera on 14/5/22.
+//
+
+import Foundation
